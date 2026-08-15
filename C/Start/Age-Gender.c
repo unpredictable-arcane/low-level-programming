efb723b13a3615cd15bd2dfd373e4ecb36c0c100
unpredictable-arcane <arcane.anuj@gmail.com>
2026-08-15 15:03:12 +0530

    Remove .gitignore from staging and re-add all files

---
+++ C/Start/Age-Gender.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Full Name: Brad Pitt\n");
+	printf("Age: 55\n");
+	printf("Gender: Male\n");
+}
---
+++ C/Start/Hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("First Name: Bill \nLast Name: Clinton\n");
+	return 0;
+}
