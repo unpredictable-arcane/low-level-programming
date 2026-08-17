0b2c4a4b9c4d1ac888d65173a8850d50c0eb2e06
unpredictable-arcane <arcane.anuj@gmail.com>
2026-08-17 16:37:59 +0530

    Add Basic calculator

---
+++ C/Start/Basic-Calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d + %d = %d \n", 5, 2, 5 + 2);
+
+	printf("%d - %d = %d \n", 5, 2, 5 - 2);
+
+	printf("%d * %d = %d \n", 5, 2, 5 * 2);
+
+	printf("%d / %d = %d \n", 5, 2, 5 / 2); // We take full part of the division
+
+	printf("%d %% %d = %d \n", 5, 2, 5 % 2); // Taking the remainder. Think about what would happen if you used just one "%".
+
+	return 0;
+
+}
