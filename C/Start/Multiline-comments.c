31b51ad0c48ef7ad6dad4213db2326c654f8f58f
unpredictable-arcane <arcane.anuj@gmail.com>
2026-08-17 16:38:59 +0530

    Add Multiline comment

---
+++ C/Start/Multiline-comments.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	- Program to print full name
+	  and calculate the average
+	  grade of a student
+	- Printing out functionality
+	  is done
+	- Calculating the average grade
+	  should be completed later on
+	- Date
+	- Etc
+	*/
+
+	printf("Billo Bagge\n");
+	return 0;
+}
